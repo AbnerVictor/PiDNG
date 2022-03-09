6f6db0c81a9168f53f4ac543ecec9e25a29e3aea
AbnerVictor <victoryyxfei@gmail.com>
2022-03-09 16:09:04 +0800

    通过替换Tile数据实现了内容的更改

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ if __name__ == '__main__':
     # dng_path = '../extras/IMG_4155_dummy.dng'
 
     # LOAD NPY
-    data = np.load('extras/raw_deblur/M03-1318_000065.npy')
+    data = np.load('extras/raw_deblur/M03-1318_000065.npy')[0, ...]
     print(data.shape)
+
+    plt.figure()
+    plt.title('deblur')
+    plt.imshow(data, cmap='gray')
 
     # LOAD DNG
     dng_path = 'extras/raw_deblur/M03-1318_000065.dng'
@@ -21,8 +25,23 @@ if __name__ == '__main__':
     tile = my_dng.IFDTiles[8]
     assert isinstance(tile, dngTile)
     print(tile.tileWidth, tile.tileLength, tile.byteCounts)
+    # 16bits 图像
+    raw_data = np.frombuffer(tile.data[0], dtype=np.uint16)
+    raw_data = raw_data.reshape(tile.tileLength[0], tile.tileWidth[0])
+
+    plt.figure()
+    plt.title('raw')
+    plt.imshow(raw_data, cmap='gray')
+
+    data = data.flatten().astype(np.uint16)
+    print(data.shape)
+
+    data = data.tobytes()
+
+    tile.data = [data]
+    my_dng.IFDTiles[8] = tile
 
     # WRITE DNG
-    # my_dng.write('extras/raw_deblur/M03-1318_000065_dummy.dng')
+    my_dng.write('extras/raw_deblur/M03-1318_000065_deblur.dng')
 
     plt.show(block=True)
